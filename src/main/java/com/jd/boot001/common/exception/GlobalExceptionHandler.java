ef39401dc7e7f5100977fe5fd8729636fc60b67a
zwhly986 <zwh_ly@163.com>
2024-07-27 21:29:07 +0800

    全局异常处理

--- src/main/java/com/jd/boot001/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jd/boot001/common/exception/GlobalExceptionHandler.java
@@ -1,16 +1,19 @@
 package com.jd.boot001.common.exception;
 
 import com.jd.boot001.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- * 异常处理，复合注解：
+ * 异常处理类，复合注解：
  * @ControllerAdvice
  * @ResponseBody
  */
-@RestControllerAdvice
+@Slf4j
+@ControllerAdvice // 或者@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     /**
@@ -19,6 +22,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = BusinessException.class)
     @ResponseBody
     public R bizExceptionHandler(BusinessException e) {
+//        log.
         return R.error(e.getErrorMsg());
     }
 
