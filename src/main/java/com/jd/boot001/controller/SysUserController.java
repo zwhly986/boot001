600b2228f85764c3b1cfdd237fcbd3e76f756a61
zwhly986 <zwh_ly@163.com>
2024-07-28 15:39:10 +0800

    日志配置文件

--- src/main/java/com/jd/boot001/controller/SysUserController.java
+++ src/main/java/com/jd/boot001/controller/SysUserController.java
@@ -1,6 +1,7 @@
 package com.jd.boot001.controller;
 
 import com.jd.boot001.common.R;
+import com.jd.boot001.common.exception.BusinessException;
 import com.jd.boot001.entity.SysUser;
 import com.jd.boot001.service.SysUserService;
 import lombok.extern.slf4j.Slf4j;
@@ -30,14 +31,15 @@ public class SysUserController {
     @RequestMapping(value = "/querySysUser", method = RequestMethod.GET)
     public List<SysUser> querySysUser() {
         LOGGER.info("querySysUser|用户列表查询,userId={}", "10000");
-//        log.debug("querySysUser|用户列表查询");
         return sysUserService.querySysUserList();
     }
 
     @RequestMapping("/getBusinessException")
     public R getBusinessException() {
         LOGGER.debug("getBusinessException|抛出异常测试");
-        //throw new BusinessException("400","我出错了");
+        if (true) {
+            throw new BusinessException("400", "我出错了");
+        }
         return R.success();
     }
 
